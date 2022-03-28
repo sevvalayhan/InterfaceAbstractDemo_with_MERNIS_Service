fad81ba025a1755dcdec5d99aa6263df06725515
sevval <sevval@DESKTOP-IKHJK1L>
2022-03-28 13:39:30 +0300

    Add project files.

---
+++ Adapter/MernisServiceAdapter.cs
@@ -0,0 +1,31 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo.Adapter
+{
+    public class MernisServiceAdapter : ICustomerCheckService
+    {
+        public bool CheckIfRealPerson(Customer customer)
+        {
+            MernisServiceReference.KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+
+            return client.TCKimlikNoDogrulaAsync(
+                 new TCKimlikNoDogrulaRequest
+                (new TCKimlikNoDogrulaRequestBody(
+                    Convert.ToInt64(customer.NationalityId), 
+                    customer.FirstName.ToUpper(),
+                    customer.LastName.ToUpper(), 
+                    customer.DateOfBirth.Year)))
+                .Result.Body.TCKimlikNoDogrulaResult;
+
+
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapter;
+using InterfaceAbstractDemo.Concrete;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager(new MernisServiceAdapter());
+           customerManager.Save(new Customer { CustomerId=1,FirstName="ayşe",LastName="yılmaz",DateOfBirth=new DateTime(2003,7,11),NationalityId="754143545"});
+            //Mernis sistemi eklendiği için ancak gerçek bir insanın adı soyadı , doğum tarihi ve tc kimlik numarası kullanıldığında çalışır. 
+
+
+            Console.ReadLine();
+        
+        }
+    }
+}
